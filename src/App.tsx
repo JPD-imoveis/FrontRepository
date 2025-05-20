2dea9685a69993c82cbd49189c2b403360f3706b
Pedroca12345 <pepeplays8@gmail.com>
2025-05-19 21:13:54 -0300

    Primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { Header } from "./components/Header";
+import { Logo } from "./components/Logo";
+import { Main } from "./components/Main";
+
+import "./styles/globals.css";
+import "./styles/themes.css";
+
+function App() {
+
+  return (
+    <>
+      <Header>
+        <Logo />
+      </Header>
+      <Main>
+        aaa
+      </Main>
+    </>
+  )
+}
+
+export default App
